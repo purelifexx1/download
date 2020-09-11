ed460ac01a527faeedc406e8c9b41be3b0fb3a95
purelifexx_1 <taithptnhh@gmail.com>
2020-09-11 18:42:39 +0700

    888

--- node_testing/index.js
+++ node_testing/index.js
@@ -1,12 +1,20 @@
 var express = require("express");
+var MongoClient = require('mongodb').MongoClient;
+var options = {
+    useUnifiedTopology: true,
+};
+MongoClient.connect('mongodb://localhost:27017/testdb', options, function(){
+    console.log("connect");
+})
 var app = express();
-
+console.log("ola");
+app.use("/test" ,function(req, res, next){
+    console.log("hello");
+    next();
+})
 app.get("/", function(req, res){
     res.send("hello world");
 });
-app.use(function(req, res, next){
-    console.log(req);
-    console.log(res);
-})
+
 
 app.listen(3000);
